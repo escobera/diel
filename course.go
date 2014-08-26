0bfa53d4d34f3cbd8c7da1ee8c56c85eec50763d
Rafael Barbosa <rbocosta@gmail.com>
2014-08-26 18:42:06 -0300

    Course.go, incomplete

---
+++ course.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/go-martini/martini"
+	"github.com/go-xorm/xorm"
+	"github.com/martini-contrib/render"
+)
+
+type Course struct {
+	Id           int64     `form:"id" json:"id"`
+	Name         string    `form:"name" json:"name"`
+	Registration string    `form:"registration" json:"registration"`
+	Created      time.Time `form:"created_at" json:"created_at" xorm:"CREATED"`
+}
+
+type CourseJSON struct {
+	Student Student `json:"course"`
+}
+
+func Courses(orm *xorm.Engine, r render.Render) {
+	students := make([]Courses, 0)
+	orm.Find(&students)
+	r.JSON(200, map[string]interface{}{"students": students})
+}
+
+func CreateStudent(orm *xorm.Engine, r render.Render, studentJson StudentJSON) {
+	student := studentJson.Student
+	_, err := orm.Insert(student)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	r.JSON(200, map[string]interface{}{"student": student})
+}
+
+func UpdateStudent(orm *xorm.Engine, r render.Render, params martini.Params, studentJson StudentJSON) {
+	student := studentJson.Student
+	student.Id, _ = strconv.ParseInt(params["id"], 10, 64)
+	_, err := orm.Id(student.Id).Update(&student)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	r.JSON(200, map[string]interface{}{"student": student})
+}
+
+func DeleteStudent(orm *xorm.Engine, r render.Render, params martini.Params) {
+	_, err := orm.Id(params["id"]).Delete(&Student{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	r.JSON(204, map[string]interface{}{})
+}
