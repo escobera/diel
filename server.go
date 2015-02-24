5dd9d2073c3bb138f0ee028d0bea8e795f59ff12
Rafael Barbosa <rbocosta@gmail.com>
2015-02-24 18:54:53 -0300

    Changing martinio

--- server.go
+++ server.go
@@ -2,60 +2,60 @@ package main
 
 import (
 	_ "encoding/json"
-	"fmt"
-	"net/http"
 
-	"github.com/go-martini/martini"
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
-	"github.com/martini-contrib/binding"
-	"github.com/martini-contrib/cors"
-	"github.com/martini-contrib/render"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db, err := gorm.Open("postgres", "user=rafa dbname=diel sslmode=disable")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
-	db.LogMode(true)
-	// Migrate DB
-	db.AutoMigrate(Student{})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	m := martini.Classic()
-	m.Map(&db)
-	m.Use(render.Renderer(render.Options{
-		HTMLContentType: "application/json",
-	}))
-	m.Use(cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"http://localhost:4200"},
-		AllowMethods:     []string{"PUT", "PATCH", "POST", "OPTIONS", "GET", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
+	// db, err := gorm.Open("postgres", "user=rafa dbname=diel sslmode=disable")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer db.Close()
+	// db.LogMode(true)
+	// // Migrate DB
+	// db.AutoMigrate(Student{})
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	router := gin.Default()
 
-	//Root
-	m.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		http.ServeFile(w, r, "public/index.html")
+	router.Get("/", func(c *gin.Context) {
+		c.HTML(200, "public/index.html")
 	})
 
+	router.Run(":3001")
+
+	// m.Map(&db)
+	// m.Use(render.Renderer(render.Options{
+	// 	HTMLContentType: "application/json",
+	// }))
+	// m.Use(cors.Allow(&cors.Options{
+	// 	AllowOrigins:     []string{"http://localhost:4200"},
+	// 	AllowMethods:     []string{"PUT", "PATCH", "POST", "OPTIONS", "GET", "DELETE"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// }))
+
+	//Root
+	// router.Get("/", func(c *gin.Context) {
+	// 	w.Header().Set("Content-Type", "text/html")
+	// 	http.ServeFile(w, r, "public/index.html")
+	// })
+
 	//Student
-	m.Get("/students", Students)
-	m.Post("/students", binding.Json(StudentJSON{}), CreateStudent)
-	m.Put("/students/:id", binding.Json(StudentJSON{}), UpdateStudent)
-	m.Delete("/students/:id", binding.Json(StudentJSON{}), DeleteStudent)
+	// m.Get("/students", Students)
+	// m.Post("/students", binding.Json(StudentJSON{}), CreateStudent)
+	// m.Put("/students/:id", binding.Json(StudentJSON{}), UpdateStudent)
+	// m.Delete("/students/:id", binding.Json(StudentJSON{}), DeleteStudent)
 
 	//Course
 	// m.Get("/courses", Courses)
 	// m.Post("/courses", binding.Json(CourseJSON{}), CreateCourse)
 	// m.Put("/courses/:id", binding.Json(CourseJSON{}), UpdateCourse)
 	// m.Delete("/courses/:id", binding.Json(CourseJSON{}), DeleteCourse)
-	m.Run()
+
 }
